abfa8ff58aeb30b4acadbc92239e38b868abc38b
hayato <hayato.techacademy@gmail.com>
2021-03-23 11:30:26 +0000

    add column

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_03_23_084018) do
+ActiveRecord::Schema.define(version: 2021_03_23_111714) do
 
   create_table "tasklists", options: "ENGINE=InnoDB DEFAULT CHARSET=utf8", force: :cascade do |t|
     t.string "content"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 2021_03_23_084018) do
     t.string "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "status"
   end
 
 end
