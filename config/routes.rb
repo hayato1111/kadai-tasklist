1b021bdc15fbda0b57ea3b0d6e47a760646a3a92
hayato <hayato.techacademy@gmail.com>
2021-04-10 12:04:43 +0000

    task

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  get 'users/new'
-  get 'users/create'
   root to: 'tasks#index'
   
-  resources :tasks, only: [:create, :new, :edit, :show, :update, :destroy]
+  resources :tasks
   
   get 'login', to: 'sessions#new'
   post 'login', to: 'sessions#create'
